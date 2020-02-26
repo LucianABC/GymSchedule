d5d7dfec56290ca3348ad6d71bc2bcd7e122d78d
LucianABC <louabecasis@gmail.com>
2020-02-26 13:00:35 -0300

    edition done

--- src/components/DayCard/components/Activity.js
+++ src/components/DayCard/components/Activity.js
@@ -10,9 +10,14 @@ const Activity = ({activity, dayName}) => {
     const [ableEdition, toggleEditor] = useBool(false);
     const [name, setName] = useState(activity.name);
     const [quant, setQuant] = useState(activity.quantity);
+    
+    const deleteActivity = ()=> {
+        handleActivities(dayName, "delete",activity);
+    }
+  
 
     const handleEdition =()=>{
-        toggleValue();
+  
         const nActivity={
             id: activity.id,
             name: name,
@@ -20,38 +25,46 @@ const Activity = ({activity, dayName}) => {
             done: checkboxBool
         }   
         handleActivities(dayName, "edit", nActivity);
+        
     };
 
-    const deleteActivity = ()=> {
-        handleActivities(dayName, "delete",activity);
-    }
-  
     return(
-        <div className="activity-container">
-            <span className="edit-activity">
-                <FontAwesomeIcon icon={faPen} className="icon" onClick={toggleEditor}/>              
-                <FontAwesomeIcon icon={faTrashAlt} onClick={deleteActivity} className="icon" />
-            </span>
-        {ableEdition ?
-            <div className="activity-content-editor">
-                <input type="text" className="name-input" value={name} 
-                                                        onChange={e=>{setName(e.target.value)} }/> 
-           
-                <input type="number" className="quantity-input" value={activity.quantity}
-                                                                onChange={e=>{setQuant(e.target.value)}}/> 
-            </div>
-             :
-            <div className="activity-content">
-                <span  className={checkboxBool?  null : "done" }> 
-                    {activity.name}
+        <div>
+            
+            <div className="activity-container">
+                <span className="edit-activity">
+                    <FontAwesomeIcon icon={faPen} className="icon" onClick={toggleEditor}/>              
+                    <FontAwesomeIcon icon={faTrashAlt} onClick={deleteActivity} className="icon" />
                 </span>
-                <span className={checkboxBool? null : "done" }> 
-                    {activity.quantity}
-                </span>
+            {ableEdition ?
+                <div className="activity-content-editor">
+                    <input type="text" className="name-input" value={name} 
+                                                            onChange={e=>{setName(e.target.value)} }/> 
+            
+                    <input type="number" className="quantity-input" value={quant}
+                                                                    onChange={e=>{setQuant(e.target.value)}}/> 
+                </div>
+                :
+                <div className="activity-content">
+                    <span  className={checkboxBool?  null : "done" }> 
+                        {activity.name}
+                    </span>
+                    <span className={checkboxBool? null : "done" }> 
+                        {activity.quantity}
+                    </span>
+                </div>
+            }
+                <input type="checkbox" onChange={e=>{
+                                        toggleValue();
+                                        handleEdition();}} />
             </div>
-        }
-
-            <input type="checkbox" onChange={handleEdition} />
+            {ableEdition ? 
+                <button onClick={e=>{
+                                handleEdition();
+                                toggleEditor();
+                                }}>
+                    Ok
+                </button> : null}
         </div>
     );
 }
